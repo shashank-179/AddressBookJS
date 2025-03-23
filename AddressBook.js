9bca03d5c5f5e0e9656f5a69b68bcc0041705fb9
shashank-179 <shashank17903@gmail.com>
2025-03-24 01:34:09 +0530

    [Shashank Mishra]: ADD: Addding UC2

--- AddressBook.js
+++ AddressBook.js
@@ -4,6 +4,15 @@ const readline = require("readline");
 
 class Contact {
     constructor(firstName, lastName, address, city, state, zip, phone, email) {
+        if (!this.validateName(firstName)) throw new Error("Invalid First Name! Must start with a capital letter and have at least 3 characters.");
+        if (!this.validateName(lastName)) throw new Error("Invalid Last Name! Must start with a capital letter and have at least 3 characters.");
+        if (!this.validateAddress(address)) throw new Error("Invalid Address! Must have at least 4 characters.");
+        if (!this.validateAddress(city)) throw new Error("Invalid City! Must have at least 4 characters.");
+        if (!this.validateAddress(state)) throw new Error("Invalid State! Must have at least 4 characters.");
+        if (!this.validateZip(zip)) throw new Error("Invalid ZIP Code! Must be a 5 or 6 digit number.");
+        if (!this.validatePhone(phone)) throw new Error("Invalid Phone Number! Must be a 10-digit number.");
+        if (!this.validateEmail(email)) throw new Error("Invalid Email Address! Format should be example@domain.com");
+
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -12,6 +21,25 @@ class Contact {
         this.zip = zip;
         this.phone = phone;
         this.email = email;
+    }
+    validateName(name) {
+        return /^[A-Z][a-zA-Z]{2,}$/.test(name);
+    }
+
+    validateAddress(value) {
+        return /^[a-zA-Z0-9\s]{4,}$/.test(value);
+    }
+
+    validateZip(zip) {
+        return /^\d{5,6}$/.test(zip);
+    }
+
+    validatePhone(phone) {
+        return /^\d{10}$/.test(phone);
+    }
+
+    validateEmail(email) {
+        return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
     }
 
     display() {
